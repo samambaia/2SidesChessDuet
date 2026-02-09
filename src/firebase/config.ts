8bc8d38138b3149fffd0e5e00e89a6b65bf6d16b
2Sides AI Company <twosidessides@gmail.com>
2026-02-09 18:42:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3509208910-49f15",
+  "appId": "1:749015130136:web:8a44129a4d440d5407a36d",
+  "apiKey": "AIzaSyDW6s5ipW8gA6xki5mKDJHVWt_rsNZR2HQ",
+  "authDomain": "studio-3509208910-49f15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "749015130136"
+};
